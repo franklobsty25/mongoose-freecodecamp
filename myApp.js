db5de1995b04abfec761aab79e3a793293c42076
Frank Adu Opoku Kodie <frankkodie@yahoo.com>
2023-02-24 13:50:06 +0000

    find, edit, save

--- myApp.js
+++ myApp.js
@@ -71,8 +71,20 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = 'hamburger';
+  Person.find({ _id: personId }, function (err, person) {
+    if (err) {
+      return console.log(err);
+    }
+    person.favoriteFoods.push = foodToAdd;
 
-  done(null /*, data*/);
+    person.save(function (err, data) {
+      if (err) {
+        return console.log(err);
+      }
+
+      done(null, data);
+    });
+  });
 };
 
 const findAndUpdate = (personName, done) => {
