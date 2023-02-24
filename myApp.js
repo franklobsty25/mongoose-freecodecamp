78fde8ae531704ff6fcab6e91b8612a78b7f0911
Frank Adu Opoku Kodie <frankkodie@yahoo.com>
2023-02-24 13:36:54 +0000

    find favorite foods

--- myApp.js
+++ myApp.js
@@ -50,7 +50,13 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, function (err, data) {
+    if (err) {
+      return console.error(err);
+    }
+
+    done(null, data);
+  });
 };
 
 const findPersonById = (personId, done) => {
