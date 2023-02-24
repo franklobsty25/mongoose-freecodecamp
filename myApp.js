41d9adb17741110c5ad6bce2268ff857fc864f41
Frank Adu Opoku Kodie <frankkodie@yahoo.com>
2023-02-24 12:26:14 +0000

    schema

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-let Person = require('./models/person');
+let Person = require('./models/person.js');
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
